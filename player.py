f76c7d07910cfbfba11666e28d2a50e3a757e7d0
Tayakani <danny.pruisscher@gmail.com>
2026-02-16 23:59:28 +0100

    pew pew

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -37,6 +39,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
     def move(self, dt):
@@ -44,3 +48,12 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    
+    def shoot(self):
+        pew = Shot(self.position.x, self.position.y)
+        pew.velocity = pygame.Vector2(0, 1)
+        rotated_vector =  pew.velocity.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+        pew.velocity = rotated_with_speed_vector
+
